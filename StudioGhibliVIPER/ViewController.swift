5707f4fcbb76bf07253761b555086f6131e7a517
Mila Alfitri <alfitriv@gmail.com>
2019-09-20 07:37:52 +0700

    Initial Commit

---
+++ StudioGhibliVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudioGhibliVIPER
+//
+//  Created by Vania Radmila Alfitri on 20/09/19.
+//  Copyright © 2019 Gojek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
